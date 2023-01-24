49e9c62fb68d8fcb20ceabd38f4ab874ce396e86
abdulrahmansbq <abdulrahmansbq@gmail.com>
2023-01-24 11:16:42 +0300

    big

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,1 @@
-<a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+<h1><a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a></h1>
